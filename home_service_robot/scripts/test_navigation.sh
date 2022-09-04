f82c3b2bc0aa21ee0caf6eae8d805cb1b15e513d
Arianna Kan <kan.ari123@gmail.com>
2022-09-04 04:50:44 +0000

    partially tuned amcl

--- home_service_robot/scripts/test_navigation.sh
+++ home_service_robot/scripts/test_navigation.sh
@@ -3,9 +3,9 @@ echo "Launching my_robot world.launch"
 xterm -e "roslaunch home_service_robot world.launch" &
 sleep 10
 
-echo "Launching amcl_demo.launch"
+echo "Launching amcl.launch"
 #xterm -e "roslaunch my_robot gmapping.launch" &
-xterm -e "roslaunch turtlebot_navigation amcl_demo.launch" &
+xterm -e "roslaunch home_service_robot amcl.launch" &
 sleep 5
 
 echo "Launching turtlebot view_navigation.launch"
